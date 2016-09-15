d2c28b1f9d57ea1fddfa32604b88fb6f62365b6e
daminh <davidcodingemail@gmail.com>
2016-09-15 00:41:28 -0700

    Add files via upload
    Updated with new finished features, such as: Search Flight, Create Flight, and Edit Flights

---
+++ Martinez_Van_FlightTrackingSystem2.0/Martinez_Van_FlightTrackingSystem/Martinez_Van_FlightTrackingSystem/Flight.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomelandSecurity;
+
+namespace Martinez_Van_FlightTrackingSystem
+{
+    class Flight
+    {
+        //Instance Variables
+        private int flightNumber;
+        private string flightDestination;
+        private string flightOrigin;
+        private Passenger[] flightManifest;
+
+        //Basic Constructor
+        public Flight()
+        {}
+
+        //Main Flight Constructor
+        public Flight(int flightNumber, string flightOrigin, string flightDestination, Passenger[] flightManifest)
+        {
+            this.flightNumber = flightNumber;
+            this.flightDestination = flightDestination;
+            this.flightOrigin = flightOrigin;
+            this.flightManifest = flightManifest;
+        }
+
+        //Properties
+        public int FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+        public string FlightDestination
+        {
+            get { return flightDestination; }
+            set { flightDestination = value; }
+        }
+
+        public string FlightOrigin
+        {
+            get { return flightOrigin; }
+            set { flightOrigin = value; }
+        }
+
+        public Passenger[] FlightManifest
+        {
+            get { return flightManifest; }
+            set { flightManifest = value; }
+        }
+
+        public int PassengerCount
+        {
+            get 
+            {
+                int counter = 0;
+                for (int i = 0; i < FlightManifest.Length; i++)
+                {
+                    if(FlightManifest[i] != null)
+                    { counter++; }
+                }
+
+                return counter;
+            }
+        }
+
+
+        /**************************/
+        /*   Main Menu Option 1   */
+        /**************************/
+        public bool DisplayFlights(Flight[] flightList)
+        {
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < flightList.Length; i++)
+            {
+                if (flightList[i] != null)
+                { counter++; }
+            }
+
+            //Displays Header
+            Console.Clear();
+            Header();
+            Console.WriteLine("\t\t\t      Current Flights      \n\n");
+
+            //Displays Flights
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("{0,4}. {1,-8}{2,-20}-->     {3}\n", i + 1, flightList[i].FlightNumber, flightList[i].FlightOrigin, flightList[i].FlightDestination);
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            //Lets the application loop back to the main menu
+            return true;
+        }
+
+
+        /**************************/
+        /*   Main Menu Option 2   */
+        /**************************/
+        public Flight[] CreateFlights(Flight[] flightList)
+        {
+            string input = "";
+            int numCheck = 0;
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < flightList.Length; i++)
+            {
+                if (flightList[i] != null)
+                { counter++; }
+            }
+
+            if (counter < 20)
+            {
+                //Create Flight at next available spot in the array
+                flightList[counter] = new Flight();
+
+                //Clear Screen and Add Header
+                Console.Clear();
+                Header();
+                Console.WriteLine("\t\t\t        Add Flight        \n\n");
+
+                //Flight Number
+                numCheck = VerifyInputIsNum("Enter Flight Number between 1 and 999: ");
+                flightList[counter].FlightNumber = numCheck;
+
+                //Flight Origin
+                Console.Write("\nEnter Origin: ");
+                input = Console.ReadLine();
+                flightList[counter].FlightOrigin = input;
+
+                //Flight Destination
+                Console.Write("\nEnter Destination: ");
+                input = Console.ReadLine();
+                flightList[counter].FlightDestination = input;
+
+                //Create new Flight Manifest
+                Passenger[] newManifest = new Passenger[20];
+                flightList[counter].FlightManifest = newManifest;
+            }
+            else
+            {
+                Console.WriteLine("Flight List is full. No more flights can be added at this time.");
+            }
+
+            return flightList;
+        }
+
+
+        /**************************/
+        /*   Main Menu Option 3   */
+        /**************************/
+        public int SelectFlights(Flight[] flightList)
+        {
+            /**************************/
+            /*Taken from DisplayFlight*/
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < flightList.Length; i++)
+            {
+                if (flightList[i] != null)
+                { counter++; }
+            }
+
+            //Displays Header
+            Console.Clear();
+            Header();
+            Console.WriteLine("\t\t\t      Current Flights      \n\n");
+
+            //Displays Flights
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("{0,4}. {1,-8}{2,-20}-->     {3}\n", i + 1, flightList[i].FlightNumber, flightList[i].FlightOrigin, flightList[i].FlightDestination);
+            }
+            /**************************/
+
+            int numCheck = 0;
+            bool continueLoop = true;
+            
+            do{
+                //Ask for flight and verify number
+                numCheck = VerifyInputIsNum("Enter Option Number: ");
+
+                if (numCheck < 1 || numCheck > (counter + 1))
+                { continueLoop = true; }
+                else
+                { continueLoop = false; }
+            } while (continueLoop == true);
+
+            //Return the number of the flight selected minus 1 to get the array index
+            return numCheck - 1;
+        }
+
+
+        /**************************/
+        /*   Main Menu Option 4   */
+        /**************************/
+        public bool SearchFlights(Flight[] flightList)
+        {
+            string fName = "";
+            string lName = "";
+            int counter = 0;
+            bool record = false;
+            bool header = false;
+
+            //Displays Header
+            Console.Clear();
+            Header();
+            Console.WriteLine("\t\t\t      Find Passenger      \n\n");
+
+            Console.Write("Enter Passenger First Name: ");
+            fName = Console.ReadLine();
+            Console.Write("Enter Passenger Last Name: ");
+            lName = Console.ReadLine();
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < flightList.Length; i++)
+            {
+                if (flightList[i] != null)
+                { counter++; }
+            }
+
+            for (int i = 0; i < counter; i++)
+            {
+
+                for (int j = 0; j < flightList[i].PassengerCount; j++)
+                {
+
+                    if (((flightList[i].FlightManifest[j].FirstName.ToLower()) == fName.ToLower()) && ((flightList[i].FlightManifest[j].LastName.ToLower()) == lName.ToLower()))
+                    {
+                        if (header == false)
+                        {
+                            Console.WriteLine("\nFlight   First Name     Last Name     Loyalty Number\n");
+                            header = true;
+                        }
+                        Console.WriteLine("{0,-9}{1,-15}{2,-15}{3,-19}", flightList[i].FlightNumber, flightList[i].FlightManifest[j].FirstName, flightList[i].FlightManifest[j].LastName, flightList[i].FlightManifest[j].LoyaltyNumber);
+                        record = true;
+                    }                   
+                }
+            }
+
+            if(record == false)
+            { Console.WriteLine("\nNo Records Found."); }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            //Lets the application loop back to the main menu
+            return true;
+        }
+
+
+        /**************************/
+        /*    SubMenu Option 1    */
+        /**************************/
+        public bool DisplayPassenger(Flight[] flightList, int flightIndex)
+        {
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < (flightList[flightIndex].FlightManifest).Length; i++)
+            {
+                if (flightList[flightIndex].FlightManifest[i] != null)
+                { counter++; }
+            }
+
+            //Displays Header
+            Console.Clear();
+            Header();
+            Console.WriteLine("\t\t\t    Current Passengers     \n\n");
+
+            //Displays Passengers
+            Console.WriteLine("Number   First Name     Last Name     Loyalty Number     Flagged\n\n");
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("{0,-9}{1,-15}{2,-15}{3,-19}{4}", i + 1, flightList[flightIndex].FlightManifest[i].FirstName, flightList[flightIndex].FlightManifest[i].LastName, flightList[flightIndex].FlightManifest[i].LoyaltyNumber, flightList[flightIndex].FlightManifest[i].SecurityFlag);
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            //Lets the application loop back to the sub menu
+            return true;
+        }
+
+
+        /**************************/
+        /*    SubMenu Option 2    */
+        /**************************/
+        public bool EditFlight(Flight[] flightList, int flightIndex)
+        {
+            string input = "";
+            int numCheck = 0;
+
+            //Displays Header
+            Console.Clear();
+            Header();
+            Console.WriteLine("\t\tEdit Flight Information - Flight {0}\n\n", flightList[flightIndex].FlightNumber);
+            Console.WriteLine("\n{0,-8}{1,-20}-->     {2}\n\n", flightList[flightIndex].FlightNumber, flightList[flightIndex].FlightOrigin, flightList[flightIndex].FlightDestination);
+
+            //Flight Number
+            numCheck = VerifyInputIsNum("Enter New Flight Number between 1 and 999: ");
+            flightList[flightIndex].FlightNumber = numCheck;
+
+            //Flight Origin
+            Console.Write("\nEnter New Origin: ");
+            input = Console.ReadLine();
+            flightList[flightIndex].FlightOrigin = input;
+
+            //Flight Destination
+            Console.Write("\nEnter New Destination: ");
+            input = Console.ReadLine();
+            flightList[flightIndex].FlightDestination = input;
+
+
+            Console.WriteLine("Flight information updated...");
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            //Lets the application loop back to the sub menu
+            return true;
+        }
+
+
+        /**************************/
+        /*    SubMenu Option 3    */
+        /**************************/
+        public Passenger[] AddPassenger(Flight[] flightList, int flightIndex)
+        {
+            string input = "";
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < (flightList[flightIndex].FlightManifest).Length; i++)
+            {
+                if (flightList[flightIndex].FlightManifest[i] != null)
+                { counter++; }
+            }
+
+            if (counter < 20)
+            {
+                //Create Flight at next available spot in the array
+                flightList[flightIndex].FlightManifest[counter] = new Passenger();
+
+                //Clear Screen and Add Header
+                Console.Clear();
+                Header();
+                Console.WriteLine("\t\t\t      Add Passenger       \n\n");
+
+                //Passenger First Name
+                Console.Write("\nEnter Passenger First Name: ");
+                input = Console.ReadLine();
+                flightList[flightIndex].FlightManifest[counter].FirstName = input;
+
+                //Passenger Last Name
+                Console.Write("\nEnter Passenger Last Name: ");
+                input = Console.ReadLine();
+                flightList[flightIndex].FlightManifest[counter].LastName = input;
+
+                //Passenger Loyalty Number
+                Console.Write("\nEnter Passenger Loyalty Number: ");
+                input = Console.ReadLine();
+                flightList[flightIndex].FlightManifest[counter].LoyaltyNumber = input; ;
+            }
+            else
+            {
+                Console.WriteLine("Passenger List is full. No more passengers can be added at this time.");
+            }
+
+            return flightList[flightIndex].FlightManifest;
+        }
+
+
+        /**************************/
+        /*    SubMenu Option 4    */
+        /**************************/
+        public bool RunSecurity(Flight[] flightList, int flightIndex)
+        {
+            FlaggedPassenger[] flaggedList = new FlaggedPassenger[20];
+            TSA security = new TSA();
+            flaggedList = security.RunSecurityCheck(flightList[flightIndex].FlightManifest, flightList[flightIndex].PassengerCount);
+            
+            int counter = 0;
+
+            //Counts how many elements in the array are filled
+            for (int i = 0; i < flaggedList.Length; i++)
+            {
+                if (flaggedList[i] != null)
+                { counter++; }
+            }
+
+            //Loop through each flagged passenger, comparing their name to the flight manifest, and flagging them if equal
+            for (int i = 0; i < counter; i++)
+            {
+                for(int j = 0; j < flightList[flightIndex].PassengerCount; j++)
+                {
+                    if (((flightList[flightIndex].FlightManifest[j].FirstName) == flaggedList[i].FirstName) && ((flightList[flightIndex].FlightManifest[j].LastName) == flaggedList[i].LastName))
+                    {
+                        flightList[flightIndex].FlightManifest[i].SecurityFlag = true;
+                    }
+                }
+            }
+            
+            Console.WriteLine("\nSecurity Check Complete. Manifest updated.");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            //Lets the application loop back to the sub menu
+            return true;
+        }
+
+
+        /***************************/
+        /*      Header Method      */
+        /***************************/
+        public void Header()
+        {
+            Console.WriteLine("\t\t\tMartinez-Van Flight System");
+            Console.WriteLine("\t\t\t       --- MVFS ---       \n");
+        }
+
+        /***************************/
+        /*   Verify Input Method   */
+        /***************************/
+        private int VerifyInputIsNum(string question)
+        {
+            bool loop = true;
+            string input = "";
+            int numCheck = 0;
+
+            do //Tests if User enters a numeric value
+            {
+                Console.Write(question);
+                input = Console.ReadLine();
+
+                try
+                {
+                    numCheck = Convert.ToInt32(input);
+                    loop = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You did not enter a number. Try Again\n");  
+                }
+            } while (loop == true);
+
+            return numCheck;
+        }//End of Verify Input Method
+    }
+}
---
+++ Martinez_Van_FlightTrackingSystem2.0/Martinez_Van_FlightTrackingSystem/Martinez_Van_FlightTrackingSystem/FlightSystem.cs
@@ -0,0 +1,231 @@
+﻿// Project Alpha, Devin Martinez, CIS 345, Tuesday 1:30 PM
+// Project Alpha, David Van, CIS 345, Tuesday 1:30 PM
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomelandSecurity;
+
+namespace Martinez_Van_FlightTrackingSystem
+{
+    class FlightSystem
+    {
+        static void Main(string[] args)
+        {
+            //Start Application
+            DisplayMainMenu();
+        }
+
+
+        /********************************/
+        /*   Display Main Menu Method   */
+        /********************************/
+        static private void DisplayMainMenu()
+        {
+            //Variables
+            int input = 0;
+            bool loop = true;
+            int selectedFlight = 0;
+
+            //Create basic temp flight to call methods if needed
+            Flight temp = new Flight();
+
+            //Create Flight Array to hold Flights. Given 20 elements to hold new entries.
+            Flight[] flightList = new Flight[20];
+
+            //Premade Flight Manifests
+            Passenger[] flightManifest1 = new Passenger[20];
+            Passenger[] flightManifest2 = new Passenger[20];
+            Passenger[] flightManifest3 = new Passenger[20];
+
+            //Premade Passengers
+            Passenger passenger1 = new Passenger("Souma", "Yukihira", "111111");
+            Passenger passenger2 = new Passenger("Erina", "Nakiri", "131313");
+            Passenger passenger3 = new Passenger("Megumi", "Katou", "630274");
+            Passenger passenger4 = new Passenger("Utaha", "Kasumigaoka", "172037");
+            Passenger passenger5 = new Passenger("Eriri", "Sawamura", "936104");
+            Passenger passenger6 = new Passenger("Yukino", "Yukinoshita", "777777");
+            Passenger passenger7 = new Passenger("Yui", "Yuigahama", "190273");
+            Passenger passenger8 = new Passenger("Tony", "Calzone", "854920");
+            Passenger passenger9 = new Passenger("Ricki", "Stars", "131313");
+            Passenger passenger10 = new Passenger("Devin", "Martinez", "666666");
+            Passenger passenger11 = new Passenger("David", "Van", "999999");
+
+            //Premade Flights
+            Flight flight1 = new Flight(578, "Phoenix", "Tokyo", flightManifest1);
+            Flight flight2 = new Flight(430, "San Diego", "New York", flightManifest2);
+            Flight flight3 = new Flight(724, "Seattle", "Phoenix", flightManifest3);
+
+            //Store Passengers in Flight 1
+            flightManifest1[0] = passenger8;    //Tony C.
+            flightManifest1[1] = passenger9;    //Ricki S.
+            flightManifest1[2] = passenger10;   //Devin M.
+            flightManifest1[3] = passenger11;   //David V.
+            flightManifest1[4] = passenger3;    //Megumi K.
+            flightManifest1[5] = passenger4;    //Utaha K.
+            flightManifest1[6] = passenger5;    //Eriri S.
+
+            //Store Passengers in Flight 2
+            flightManifest2[0] = passenger3;    //Megumi K.
+            flightManifest2[1] = passenger4;    //Utaha K.
+            flightManifest2[2] = passenger5;    //Eriri S.
+            flightManifest2[3] = passenger1;    //Souma Y.
+            flightManifest2[4] = passenger2;    //Erina N.
+            flightManifest2[5] = passenger6;    //Yukino Y.
+            flightManifest2[6] = passenger7;    //Yui Y.
+
+            //Store Passengers in Flight 3
+            flightManifest3[0] = passenger1;    //Souma Y.
+            flightManifest3[1] = passenger2;    //Erina N.
+            flightManifest3[2] = passenger6;    //Yukino Y.
+            flightManifest3[3] = passenger7;    //Yui Y.
+            flightManifest3[4] = passenger8;    //Tony C.
+            flightManifest3[5] = passenger9;    //Ricki S.
+            flightManifest3[6] = passenger10;   //Devin M.
+            flightManifest3[7] = passenger11;   //David V.
+
+            //Store Flights in Flight List
+            flightList[0] = flight1;    //Phoenix to Tokyo
+            flightList[1] = flight2;    //San Diego to NY
+            flightList[2] = flight3;    //Seattle to Phoenix
+
+
+            /******************************/
+            /*          Main Menu         */
+            /******************************/
+            do
+            {
+                //Display Menu
+                Console.Clear();
+                Console.WriteLine("\t\t*** Weclome to the Martinez-Van Flight System ***");
+                Console.WriteLine("\t\t-------------------------------------------------\n\n");
+                Console.WriteLine("1) Display a List of Flights");
+                Console.WriteLine("2) Create New Flights");
+                Console.WriteLine("3) Select a Flight");
+                Console.WriteLine("4) Search by Passenger");
+                Console.WriteLine("5) Exit\n");
+
+                //Takes user input to Verify it is of correct type and within Range and returns the value
+                input = VerifyMenuEntry();
+
+                //Menu Options
+                switch (input)
+                {
+                    case 1:
+                        //Displays Flights. Takes a bool to loop back to the Main Menu
+                        loop = temp.DisplayFlights(flightList);
+                        break;
+                    case 2:
+                        //Create new flights
+                        flightList = temp.CreateFlights(flightList);
+                        break;
+                    case 3:
+                        //Select a flight
+                        selectedFlight = temp.SelectFlights(flightList);
+                        DisplaySubMenu(flightList, selectedFlight, temp);
+                        break;
+                    case 4:
+                        //Search all flights for a particular passenger by first and last name
+                        loop = temp.SearchFlights(flightList);
+                        break;
+                    case 5:
+                        //Exits the program
+                        loop = false;
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        break;
+                }
+
+            } while (loop == true);
+        }//End of Display Main Menu Method
+
+        /******************************/
+        /*   Display SubMenu Method   */
+        /******************************/
+        static private void DisplaySubMenu(Flight[] flightList, int flightIndex, Flight temp)
+        {
+            //Variables
+            int input;
+            bool loop = true;
+
+            do
+            {
+                //Display Menu
+                Console.Clear();
+                Console.WriteLine("\t***Weclome to the Martinez-Van Flight System Sub-Menu***");
+                Console.WriteLine("\t--------------------------------------------------------");
+                Console.WriteLine("\n\t\t        Flight Menu - Flight {0}", flightList[flightIndex].FlightNumber);
+                Console.WriteLine("\n\t      {0,-8}{1,-20}-->     {2}\n\n", flightList[flightIndex].FlightNumber, flightList[flightIndex].FlightOrigin, flightList[flightIndex].FlightDestination);
+                Console.WriteLine("1) Display Passenger Manifest");
+                Console.WriteLine("2) Edit Flight Information");
+                Console.WriteLine("3) Add New Passengers");
+                Console.WriteLine("4) Run Passenger Security Check");
+                Console.WriteLine("5) Exit to Main Menu.\n");
+
+                input = VerifyMenuEntry();
+
+                //Menu Options
+                switch (input)
+                {
+                    case 1:
+                        //Display Passenger list
+                        loop = temp.DisplayPassenger(flightList, flightIndex);
+                        break;
+                    case 2:
+                        //Edit flight info
+                        loop = temp.EditFlight(flightList, flightIndex);
+                        break;
+                    case 3:
+                        //Add new passengers
+                        flightList[flightIndex].FlightManifest = temp.AddPassenger(flightList, flightIndex);
+                        break;
+                    case 4:
+                        //Run Security Check
+                        loop = temp.RunSecurity(flightList, flightIndex);
+                        break;
+                    case 5:
+                        loop = false;
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (loop == true);
+        }//End of Display SubMenu Method 
+
+
+        /***************************/
+        /*   Verify Input Method   */
+        /***************************/
+        static private int VerifyMenuEntry()
+        {
+            bool loop = true;
+            string input = "";
+            int numCheck = 0;
+
+            do //Tests if User enters a numeric value
+            {
+                Console.Write("Please enter a number (1 to 5): ");
+                input = Console.ReadLine();
+
+                try
+                {
+                    numCheck = Convert.ToInt32(input);
+
+                    //Tests if user enters a non-menu value
+                    if (numCheck < 1 || numCheck > 5)
+                    {
+                        Console.WriteLine("Invalid number. Try Again.\n");
+                    }
+                    else { loop = false; }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You did not enter a number. Try Again.\n");
+                }
+            } while (loop == true);
+
+            return numCheck;
+        }//End of Verify Input Method
+    }
+}
